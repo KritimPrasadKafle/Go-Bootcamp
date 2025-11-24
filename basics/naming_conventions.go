f28f3544e396779333cdd5199b07a2cabeb4c282
kritimprasadkafle <kritim10k@gmail.com>
2025-11-24 10:50:40 +0545

    Naming conventions and constants

---
+++ basics/naming_conventions.go
@@ -0,0 +1,31 @@
+// package main
+
+// import "fmt"
+
+// type Employee struct {
+// 	FirstName string
+// 	LastName  string
+// 	Age       int
+// }
+
+// func main() {
+// 	//PascalCase
+// 	//E.g. CalculateArea, UserInfo, NewHTTPRequest
+// 	//Structs, interfaces, enums
+
+// 	//snake_case
+// 	//E.g. user_id, first_name, http_request
+
+// 	//UPPERCASE
+// 	//Use case is Constants
+
+// 	//mixedCase
+// 	//E.g. javascript, htmlDocument, isValid
+
+// 	const MAXRETRIES = 5
+
+// 	var employeeID = 1001
+
+// 	fmt.Println("EmployeeId:", employeeID)
+
+// }
--- basics/variables.go
+++ basics/variables.go
@@ -1,25 +1,25 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-var middlename string = "Cane"
+// var middlename string = "Cane"
 
-func main() {
-	var age int
-	// var name string = "John"
-	// var name1 = "Jane"
+// func main() {
+// 	var age int
+// 	// var name string = "John"
+// 	// var name1 = "Jane"
 
-	// count := 10
-	// lastName := "Smith"
+// 	// count := 10
+// 	// lastName := "Smith"
 
-	//Default values
-	//Numeric Types : 0
-	//Boolean Types : false
-	//String Type: ""
-	//Pointers, slice, maps, functions, and structs: nil
+// 	//Default values
+// 	//Numeric Types : 0
+// 	//Boolean Types : false
+// 	//String Type: ""
+// 	//Pointers, slice, maps, functions, and structs: nil
 
-	fmt.Println(age)
+// 	fmt.Println(age)
 
-	fmt.Println(middlename)
+// 	fmt.Println(middlename)
 
-}
+// }
